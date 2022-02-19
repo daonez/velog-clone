ce17918cf7bcd5f391bd81bca4d58b015491432f
daonez <cyongchoi@gmail.com>
2022-02-19 14:40:46 +0900

    process.env로 DB연결성공

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const express = require('express')
+require("dotenv").config()
+const express = require("express")
 const app = express()
 
+//body 읽기
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 // sql 연결
-const { sequelize } = require('./models');
-sequelize.sync({ force: false })
-    .then(() => {
-        console.log('데이터베이스 연결 성공');
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-
-
+const { sequelize } = require("./models")
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터베이스 연결 성공")
+  })
+  .catch((err) => {
+    console.error(err)
+  })
 
 app.listen(3000, () => {
-    console.log('서버 실행~~')
+  console.log("서버 실행~~")
 })
